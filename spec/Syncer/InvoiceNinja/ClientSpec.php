c17d9bce7f8fb434d2a0662cbd5462f333f217cc
matthieucalie <matthieu.calie@wijs.be>
2016-12-17 21:46:55 +0100

    spec tests added

---
+++ spec/Syncer/InvoiceNinja/ClientSpec.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace spec\Syncer\InvoiceNinja;
+
+use JMS\Serializer\SerializerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Syncer\Dto\InvoiceNinja\Task;
+use Syncer\InvoiceNinja\Client;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ClientSpec extends ObjectBehavior
+{
+    function let(\GuzzleHttp\Client $client, SerializerInterface $serializer)
+    {
+        $this->beConstructedWith($client, $serializer, 'token');
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(Client::class);
+    }
+
+    function it_saves_a_new_task(\GuzzleHttp\Client $client, SerializerInterface $serializer, ResponseInterface $response)
+    {
+        $task = new Task();
+        $response->getBody()->shouldBeCalled()->willReturn('body');
+
+        $serializer->serialize($task, 'json')->shouldBeCalled();
+        $client->request('POST', 'v1/tasks', Argument::any())->shouldBeCalled()->willReturn($response);
+
+        $this->saveNewTask($task)->shouldReturn('body');
+
+    }
+}
